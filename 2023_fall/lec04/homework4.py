7380deb5460434060a5ec38cf0fb624f14ab8636
qiji2000 <834845257@qq.com>
2023-11-06 10:59:18 +0900

    Update homework4.py

--- 2023_fall/lec04/homework4.py
+++ 2023_fall/lec04/homework4.py
@@ -5,6 +5,15 @@ def list_to_dict(input_list):
     `input_list` is a value, and the corresponding key is the numerical 
     index of that element in `input_list`. 
     '''
+    result_dict = {}
+    for index, value in enumerate(input_list):
+        result_dict[index] = value
+    return result_dict
+
+print(list_to_dict([1, 3.14, "hello", True]))
+print(list_to_dict(["a", "a", "a"]))
+print(list_to_dict([]))
+
     pass
 
 
